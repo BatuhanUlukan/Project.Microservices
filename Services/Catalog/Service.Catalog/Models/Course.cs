4710d687ecdb24dd087a691da8bcbfe4ac626b02
Batuhan Ulukan <batuhanulukan_55@protonmail.com>
2023-05-12 15:09:44 +0300

    Add project files.

---
+++ Services/Catalog/Service.Catalog/Models/Course.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace Service.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime dateTime { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
